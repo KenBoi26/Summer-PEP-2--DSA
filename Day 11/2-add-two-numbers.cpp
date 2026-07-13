3d7a4aea092a14a74e15048ac6c3837090d26efc
KenBoi26 <kennethphilip23@lpu.in>
2026-07-13 10:10:31 +0530

    Fix formatting in addTwoNumbers function for consistency

--- Day 11/2-add-two-numbers.cpp
+++ Day 11/2-add-two-numbers.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-         ListNode* dummy = new ListNode(0);
+        ListNode* dummy = new ListNode(0);
         ListNode* curr = dummy;
 
         int carry = 0;
